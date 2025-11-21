bcd438af245b5fcfda7f333cc8557ae440ab7e6e
Yannick Lang <layaxx@gmx.de>
2025-11-21 13:47:39 +0100

    feat: refactor login form

--- src/app/(auth)/components/LoginForm.tsx
+++ src/app/(auth)/components/LoginForm.tsx
@@ -1,14 +1,16 @@
 "use client"
-import {AuthenticationError, PromiseReturnType} from "blitz"
+import { AuthenticationError, PromiseReturnType } from "blitz"
 import Link from "next/link"
-import {LabeledTextField} from "src/app/components/LabeledTextField"
-import {Form, FORM_ERROR} from "src/app/components/Form"
+import { LabeledTextField } from "src/app/components/LabeledTextField"
+import { Form, FORM_ERROR } from "src/app/components/Form"
 import login from "../mutations/login"
-import {Login} from "../validations"
-import {useMutation} from "@blitzjs/rpc"
-import {useSearchParams} from "next/navigation"
-import {useRouter} from "next/navigation"
-import type {Route} from "next"
+import { Login } from "../validations"
+import { useMutation } from "@blitzjs/rpc"
+import { useSearchParams } from "next/navigation"
+import { useRouter } from "next/navigation"
+import type { Route } from "next"
+import { cn } from "@/lib/utils"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 
 type LoginFormProps = {
   onSuccess?: (user: PromiseReturnType<typeof login>) => void
@@ -20,12 +22,10 @@ export const LoginForm = (props: LoginFormProps) => {
   const next = useSearchParams()?.get("next")
   return (
     <>
-      <h1>Login</h1>
-
       <Form
         submitText="Login"
         schema={Login}
-        initialValues={{email: "", password: ""}}
+        initialValues={{ email: "", password: "" }}
         onSubmit={async (values) => {
           try {
             await loginMutation(values)
@@ -37,7 +37,7 @@ export const LoginForm = (props: LoginFormProps) => {
             }
           } catch (error: any) {
             if (error instanceof AuthenticationError) {
-              return {[FORM_ERROR]: "Sorry, those credentials are invalid"}
+              return { [FORM_ERROR]: "Sorry, those credentials are invalid" }
             } else {
               return {
                 [FORM_ERROR]:
@@ -47,14 +47,29 @@ export const LoginForm = (props: LoginFormProps) => {
           }
         }}
       >
-        <LabeledTextField name="email" label="Email" placeholder="Email" />
-        <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
-        <div>
-          <Link href={"/forgot-password"}>Forgot your password?</Link>
+        <div className="flex flex-col gap-6 mb-4">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login to your account</CardTitle>
+              <CardDescription>Enter your email below to login to your account</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <LabeledTextField name="email" label="Email" placeholder="Email" />
+              <LabeledTextField
+                name="password"
+                label="Password"
+                placeholder="Password"
+                type="password"
+              />
+              <div>
+                <Link href={"/forgot-password"}>Forgot your password?</Link>
+              </div>
+            </CardContent>
+          </Card>
         </div>
       </Form>
 
-      <div style={{marginTop: "1rem"}}>
+      <div style={{ marginTop: "1rem" }}>
         Or <Link href="/signup">Sign Up</Link>
       </div>
     </>
--- src/app/(auth)/components/LogoutButton.tsx
+++ src/app/(auth)/components/LogoutButton.tsx
@@ -2,20 +2,21 @@
 import logout from "../mutations/logout"
 import { useRouter } from "next/navigation"
 import { useMutation } from "@blitzjs/rpc"
+import { Button } from "@/components/ui/button"
 
 export function LogoutButton() {
   const router = useRouter()
   const [logoutMutation] = useMutation(logout)
   return (
     <>
-      <button
+      <Button
         onClick={async () => {
           await logoutMutation()
           router.refresh()
         }}
       >
         Logout
-      </button>
+      </Button>
     </>
   )
 }
